1783a7063bbab29c5863bb697b9a758d48f69fd0
Ferdinand Kasper <ferdinand.kasper@gmx.at>
2021-12-15 20:25:59 +0100

    Reformat code

--- src/enum.ts
+++ src/enum.ts
@@ -1,18 +1,18 @@
-/* Copied from https://github.com/mchehab/zbar/blob/5b3c33d5c87f58633b550d592f6eb8e4fd580b1e/include/zbar.h */
+/* Copied from https://github.com/mchehab/zbar, release 0.23.90 */
 
 export enum ZBarSymbolType {
-  ZBAR_NONE	     = 0,   /**< no symbol decoded */
+  ZBAR_NONE	       = 0,   /**< no symbol decoded */
   ZBAR_PARTIAL     = 1,   /**< intermediate status */
-  ZBAR_EAN2	     = 2,   /**< GS1 2-digit add-on */
-  ZBAR_EAN5	     = 5,   /**< GS1 5-digit add-on */
-  ZBAR_EAN8	     = 8,   /**< EAN-8 */
-  ZBAR_UPCE	     = 9,   /**< UPC-E */
+  ZBAR_EAN2	       = 2,   /**< GS1 2-digit add-on */
+  ZBAR_EAN5	       = 5,   /**< GS1 5-digit add-on */
+  ZBAR_EAN8	       = 8,   /**< EAN-8 */
+  ZBAR_UPCE	       = 9,   /**< UPC-E */
   ZBAR_ISBN10	     = 10,  /**< ISBN-10 (from EAN-13). @since 0.4 */
-  ZBAR_UPCA	     = 12,  /**< UPC-A */
+  ZBAR_UPCA	       = 12,  /**< UPC-A */
   ZBAR_EAN13	     = 13,  /**< EAN-13 */
   ZBAR_ISBN13	     = 14,  /**< ISBN-13 (from EAN-13). @since 0.4 */
   ZBAR_COMPOSITE   = 15,  /**< EAN/UPC composite */
-  ZBAR_I25	     = 25,  /**< Interleaved 2 of 5. @since 0.4 */
+  ZBAR_I25	       = 25,  /**< Interleaved 2 of 5. @since 0.4 */
   ZBAR_DATABAR     = 34,  /**< GS1 DataBar (RSS). @since 0.11 */
   ZBAR_DATABAR_EXP = 35,  /**< GS1 DataBar Expanded. @since 0.11 */
   ZBAR_CODABAR     = 38,  /**< Codabar. @since 0.11 */
@@ -51,29 +51,29 @@ export enum ZBarSymbolType {
 }
 
 export enum ZBarConfigType {
-  ZBAR_CFG_ENABLE = 0, /**< enable symbology/feature */
-  ZBAR_CFG_ADD_CHECK,	 /**< enable check digit when optional */
-  ZBAR_CFG_EMIT_CHECK, /**< return check digit when present */
-  ZBAR_CFG_ASCII,	 /**< enable full ASCII character set */
-  ZBAR_CFG_BINARY,	 /**< don't convert binary data to text */
-  ZBAR_CFG_NUM,	 /**< number of boolean decoder configs */
+  ZBAR_CFG_ENABLE = 0,         /**< enable symbology/feature */
+  ZBAR_CFG_ADD_CHECK,	         /**< enable check digit when optional */
+  ZBAR_CFG_EMIT_CHECK,         /**< return check digit when present */
+  ZBAR_CFG_ASCII,	             /**< enable full ASCII character set */
+  ZBAR_CFG_BINARY,	           /**< don't convert binary data to text */
+  ZBAR_CFG_NUM,	               /**< number of boolean decoder configs */
 
-  ZBAR_CFG_MIN_LEN = 0x20, /**< minimum data length for valid decode */
-  ZBAR_CFG_MAX_LEN,	     /**< maximum data length for valid decode */
+  ZBAR_CFG_MIN_LEN = 0x20,     /**< minimum data length for valid decode */
+  ZBAR_CFG_MAX_LEN,	           /**< maximum data length for valid decode */
 
   ZBAR_CFG_UNCERTAINTY = 0x40, /**< required video consistency frames */
 
-  ZBAR_CFG_POSITION = 0x80, /**< enable scanner to collect position data */
-  ZBAR_CFG_TEST_INVERTED,   /**< if fails to decode, test inverted */
+  ZBAR_CFG_POSITION = 0x80,    /**< enable scanner to collect position data */
+  ZBAR_CFG_TEST_INVERTED,      /**< if fails to decode, test inverted */
 
-  ZBAR_CFG_X_DENSITY = 0x100, /**< image scanner vertical scan density */
-  ZBAR_CFG_Y_DENSITY,		/**< image scanner horizontal scan density */
+  ZBAR_CFG_X_DENSITY = 0x100,  /**< image scanner vertical scan density */
+  ZBAR_CFG_Y_DENSITY,		       /**< image scanner horizontal scan density */
 }
 
 export enum ZBarOrientation {
-  ZBAR_ORIENT_UNKNOWN = -1, /**< unable to determine orientation */
-  ZBAR_ORIENT_UP,	      /**< upright, read left to right */
-  ZBAR_ORIENT_RIGHT,	      /**< sideways, read top to bottom */
-  ZBAR_ORIENT_DOWN,	      /**< upside-down, read right to left */
-  ZBAR_ORIENT_LEFT,	      /**< sideways, read bottom to top */
+  ZBAR_ORIENT_UNKNOWN = -1,    /**< unable to determine orientation */
+  ZBAR_ORIENT_UP,	             /**< upright, read left to right */
+  ZBAR_ORIENT_RIGHT,	         /**< sideways, read top to bottom */
+  ZBAR_ORIENT_DOWN,	           /**< upside-down, read right to left */
+  ZBAR_ORIENT_LEFT,	           /**< sideways, read bottom to top */
 }
